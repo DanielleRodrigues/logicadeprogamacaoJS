f0af6a889c1b711fb8f9461b119954a979574e18
Danielle Rodrigues <31356718+DanielleRodrigues@users.noreply.github.com>
2022-09-11 22:08:39 -0300

    Create ex1.js
    atividade 1

---
+++ ex1.js
@@ -0,0 +1,12 @@
+/*ATIVIDADE 1
+Faça um sistema que leia a idade de uma pessoa expressa em anos, meses e dias e mostre-a expressa apenas em dias.*/
+
+const prompt=require('prompt-sync')();
+const anosvividos = prompt('Insira a quantidade de anos vividos:');
+const numAnos = Number (anosvividos);
+const mesesvividos = prompt('Insira a quantidade de meses vividos:');
+const numMeses = Number (mesesvividos);
+const diasvividos = prompt('Insira a quantidade de dias vividos:');
+const numDias = Number (diasvividos);
+const total= (numAnos*365)+ (numMeses*30.417)+ numDias
+console.log(`A quantidade de dias vividos foi: ${total}`);
