073812ee467b73d15b0ef0898e871d4ef853dbec
Danielle Rodrigues <31356718+DanielleRodrigues@users.noreply.github.com>
2022-09-12 13:02:40 -0300

    Create ex1v2.js

---
+++ lista2/ex1v2.js
@@ -0,0 +1,26 @@
+/*ATIVIDADE 1
+Faça um programa que receba três inteiros e diga qual deles é o maior.*/
+
+const prompt = require('prompt-sync')();
+const text1 = prompt('Insira o primeiro número:');
+const text2 = prompt('Insira o segundo número');
+const text3 = prompt('Insira o terceiro número');
+
+const num1 = Number (text1)
+const num2 = Number (text2)
+const num3 = Number (text3)
+
+ if (num1 > num2 && num1 > num3)
+{
+    var maior = num1
+}
+ else if (num2> num1 && num2 > num3)
+ {
+    var maior = num2
+ }
+ else 
+ {
+    var maior = num3
+ }
+
+ console.log(`O maior número é: ${maior}`); 
